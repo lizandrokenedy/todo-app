3d1ec8c52827eca073470fba95ea5fc3f5910a04
Lizandro Kenedy Maneia Santos <lizandro.santos@totemti.com.br>
2019-03-28 17:52:04 -0400

    Todo CRUD

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-       
-    </div>
-)
+export default props => {
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style='sucess' icon='check'
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                   
+                    <IconButton style='warning' icon='undo'
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>   
+                    
+                    <IconButton style='danger' icon='trash-o'
+                        onClick={() => props.handleRemove(todo)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
